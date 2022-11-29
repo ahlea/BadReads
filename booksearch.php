a51455d52a7322c5daa5ed52db916986e684b8d7
Ahlea Wright <wrightahlea@gmail.com>
2022-11-28 21:00:19 -0600

    help
    redid booksearch and added ack page

--- booksearch.php
+++ booksearch.php
@@ -1,67 +1,66 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<meta name="author" content="Ahlea Wright">
-<title>Search for a Book</title>
-<link rel="stylesheet" href="search.css">
-<!--<script src="book.js"></script>-->
+    <meta charset="UTF-8">
+    <meta name="author" content="Lindsey, Ahlea, Karis, Angie">
+
+    <title>Book Search Page</title>
+    <link rel="stylesheet" href="search.css">
 </head>
-
 <body>
-	
-	<?php
-	
-    $username = "AHLEA";
-    $password = "Lindsey";
-    $database = "CS344F22BADREADS";
+        <div>
+             <h1>Search for a Book</h1>
 
-    $con = mysqli_connect("localhost", $username, $password, $database);
-	
-    if (mysqli_connect_errno()){
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-        exit();
-    }
-	
-    if (isset($_GET['search'])) {
-    $param = "%{$_GET['search']}%";
-    $query = mysqli_prepare($con, "SELECT * FROM Results WHERE Description LIKE ?");
-    mysqli_stmt_bind_param($query, "s", $param);
-    mysqli_stmt_execute($query);
-    $results = mysqli_stmt_get_result($query);
-    $rows = mysqli_num_rows($results);
-    mysqli_stmt_close($query);
+        <form action="booksearch.php" method="POST">
+            <input placeholder="Search..." type="text" name="searchBox" value=""><br>
+            <input type="submit" name="enterSearch" value="Enter">
+        </form>
+        
+    </div>
+    <?php
+            // Report all error information on the webpage
+            error_reporting(E_ALL);
+            ini_set('display_errors', 1);
+        
+        if (isset($_POST["enterSearch"])){
+            $db_name = "CS344F22BADREADS";
+            $db_user = "AHLEA";
+            $db_passwd = "Lindsey";
 
-    if ($rows > 0) {
-        echo "<h2>Search results for: {$_GET['search']}</h2>";
-             
-        while ($result = mysqli_fetch_array($results)) {
-            $result_title=$result['Title'];
-            $result_author=$result['Author'];
-            $result_blurb=$result['Blurb'];
-			$result_rating=$result['Rating'];
-			$result_added=$result['Added'];
-				
-            echo "<div class='search_result'> 						
-                <h3><a href='$result_link'>$result_title</a></h3>
-                <article><a href='$result_author'>$result_blurb</a></article>			
-            </div>";
-        }   
-    } else {
-        echo "<h2>No results found for your search: {$_GET['search']}</h2>";
-    }
-} else {
-    echo "<h2>No search query provided. Please try your search again.</h2>";
-}
-mysqli_close();
-?>
-	
-<div class="search-box">
-    <form action="booksearch.php" method="get">
-        <input type="text" name="search" maxlength="60" placeholder="Search..." required>
-        <button type="submit"><i class="fa fa-search"></i></button>
-    </form>
-</div>
-	
+            $check = 0;
+            $db = new mysqli("localhost", $db_user, $db_passwd, $db_name);
+                          // db location,      user,    passwd, database
+            if ($db->connect_errno > 0) {
+                die('Unable to connect to database [' . $db->connect_error . ']');
+            } else {
+                $searchBox = $_POST["searchBox"];
+                $sql = "SELECT id FROM Book Search";
+                $result = $db->query($sql);
+
+                if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+//                    echo "username: " . $row["username"]. " - pass: " . $row["pass"]. " " . "<br>";
+                    if($row["id"] == $Title){
+                        $check = 1;
+                        if($row["id"] != $Title){
+                            echo '<script>alert("No books titles match that search")</script>';
+                        }
+                        else{
+                            echo "directing you to the book info page";
+                            header("Location: bookInfo.php");
+                            exit();
+                        }
+                    }
+                }
+                } else {
+                echo "0 results";
+                }
+            }   
+                $db->close();
+                echo("<p>Connection to " . $db_name . " was closed.</p>");
+        }
+
+        ?>
+        
 </body>
 </html>
