a0bbe8fc0152ace51b889bd80f94a8d7633943ca
karissa-plathe <71577471+karissa-plathe@users.noreply.github.com>
2022-11-29 14:30:51 -0600

    added menu

--- bookInfo.php
+++ bookInfo.php
@@ -10,18 +10,11 @@
     </head>
     <body>
         <header>
-            <h1> 
-                <div id="user"> Welcome, Username! &nbsp; &nbsp;</div>
-                &nbsp; &nbsp;BadReads
-                <div id="menu">
-                    <ul>
-                        <li><a href="?">Search</a></li>
-                        <li><a href="User_Profile.php">User Profile</a></li>
-                        <li><a href="?">Logout</a></li>
-                    </ul>
-                </div>
-            </h1>
+        <?php
+        include("menu.php")
+        ?>
         </header>
+        
         <?php
         // Report all error information on the webpage
         //error_reporting(E_ALL);
