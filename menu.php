fdfe58058a2aa6edaca86435dd09df1d9a526f7f
karissa-plathe <karisplath@gmail.com>
2022-11-28 21:24:39 -0600

    update :)

---
+++ menu.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang = "en">
+
+    <link rel="stylesheet" href="menu.css">
+
+    <header>
+            <h1> 
+                <div id="user">Username</div>
+                
+                <div id="title">BadReads</div>
+
+                <div id="menu">
+                    <ul>
+                        <li><a href="?">Search</a></li>
+                        <li><a href="User_Profile.php">User Profile</a></li>
+                        <li><a href="?">Logout</a></li>
+                    </ul>
+                </div>
+            </h1>
+    </header>
+
+</html>
