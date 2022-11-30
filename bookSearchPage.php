62b76721728b3cc337e5158f6d6392220e2f9d17
a-rybalko <angelikarybalko@gmail.com>
2022-11-29 20:22:41 -0600

    book search

---
+++ bookSearchPage.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Lindsey, Ahlea, Karis, Angie">
+        <title>Book Search Page</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <header>
+            <?php
+            include("menu.php")
+            ?>
+        </header>
+        <div>
+             <h1>Search for a Book</h1>
+
+            <form method="POST">
+                <input placeholder="Search..." type="text" name="searchBox" value=""><br>
+                <input type="submit" name="enterSearch" value="Enter">
+                <p id="data"></p>
+            </form>
+            <script>
+                $("form").submit(
+                    function(e){
+                        // prevent jQuery refresh the whole page after appending the new element. See the following page for more detail
+                        // https://stackoverflow.com/questions/31357050/jquerypage-refreshes-after-appending-html-with-html
+                        e.preventDefault();
+                        
+                        $.post("bookSearch.php", $(this).serialize(),
+                            function(data){ // callback function
+                                $("#data").html(data);
+                            }, 
+                        );
+                    }
+                );
+            </script>
+        </div>
+    </body>
+</html>
--- booksearch.php
+++ booksearch.php
@@ -1,72 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="author" content="Lindsey, Ahlea, Karis, Angie">
-
-    <title>Book Search Page</title>
-    <link rel="stylesheet" href="search.css">
-</head>
-<body>
-	<?php
-        include("menu.php")
-        ?>
-	
-	
-        <div>
-             <h1>Search for a Book</h1>
-
-        <form action="booksearch.php" method="POST">
-            <input placeholder="Search..." type="text" name="searchBox" value=""><br>
-            <input type="submit" name="enterSearch" value="Enter">
-        </form>
-        
-    </div>
-    <?php
-            // Report all error information on the webpage
-            error_reporting(E_ALL);
-            ini_set('display_errors', 1);
-        
-        if (isset($_POST["enterSearch"])){
-            $db_name = "CS344F22BADREADS";
-            $db_user = "AHLEA";
-            $db_passwd = "Lindsey";
-
-            
-            $db = new mysqli("localhost", $db_user, $db_passwd, $db_name);
-                          // db location,   user,    passwd,    database
-            if ($db->connect_errno > 0) {
-                die('Unable to connect to database [' . $db->connect_error . ']');
-            } else {
-                $title = $_POST["searchBox"];
-                $check = 0;
-                $sql = "SELECT Title FROM `Book Search`";
-                $result = $db->query($sql);
-
-                if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-					echo "Title: ". $row["Title"]. "<br>";
-                    if($row["Title"] == $title){
-                        $check = 1;
-                        if($row["Title"] != $title){
-                            echo '<script>alert("No books titles match that search")</script>';
-                        }
-                        else{
-                            //echo "directing you to the book info page";
-                            //header("Location: bookInfo.php");
-                            //exit();
-                        }
-                    }
-                }
-                } else {
-                echo "0 results";
-                }
-            }   
-                $db->close();
-                //echo("<p>Connection to " . $db_name . " closed.</p>");
+<?php
+    // Report all error information on the webpage
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    
+    $db_name = "CS344F22BADREADS";
+    $db_user = "AHLEA";
+    $db_passwd = "Lindsey";
+    $title = $_POST["searchBox"];
+    
+    $db = new mysqli("localhost", $db_user, $db_passwd, $db_name);
+                      // db location,   user,    passwd,    database
+    if ($db->connect_errno > 0) {
+        die('Unable to connect to database [' . $db->connect_error . ']');
+    } else {
+        $sql = "SELECT Title FROM `Book Search` WHERE Title LIKE '%" . $title . "%'";
+        $r = $db->query($sql);
+        //$result = array();
+        while ($row = $r->fetch_assoc()){ 
+            echo '<a href="bookInfo.php?id=' . $row['Title'] . '">' . $row['Title'] . '</a><br>';
         }
-
-        ?>
-        
-</body>
-</html>
+    }   
+    $db->close();
+?> 
