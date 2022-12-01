9dc5e95e5f2b8686d40a43fe67911c099b3d53ac
karissa-plathe <karisplath@gmail.com>
2022-12-01 02:32:33 -0600

    style and spacing

--- ack.php
+++ ack.php
@@ -1,80 +1,68 @@
 <!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<meta name="author" content="Lindsey, Ahlea, Karis, Angie">
-<title>Acknowledgement Page</title>
-<link rel="stylesheet" href="BookInfo.css">
-</head>
+<html style= "background-color: #8f8f8f; color: antiquewhite">
+	<head>
+		<meta charset="utf-8">
+		<meta name="author" content="Lindsey, Ahlea, Karis, Angie">
+		<title>Acknowledgement Page</title>
+		<link rel="stylesheet" href="BookInfo.css">
+	</head>
 
-<body>
-	<?php
-        include("menu.php")
-        ?>
-	
-	<div class= box>
-	<h2><u>Lindsey Arndt</u></h2>
-		<p>
-			<li>bookInfo.php</li>
-			<li>bookSearchPage.php</li>
-			<li>bookInfo.css</li>
-			<li>booksearch.php</li>
-			<li>menu.php</li>
-			
-		</p>
-	<h2><u>Ahlea Wright</u></h2>
-		<p>
-			<li>ack.php</li>
-			<li>bookInfo.css</li>
-			<li>booksearch.php</li>
-<<<<<<< Updated upstream
-			<li>login.css</li>
-=======
->>>>>>> Stashed changes
-			<li>loginPage.php</li>
-			<li>menu.php</li>
-		</p>
-	<h2><u>Karis Plath</u></h2>
-		<p>
-			<li>ack.php</li>
-			<li>bookInfo.php</li>
-			<li>booksearch.php</li>
-<<<<<<< Updated upstream
-			<li>menu.css</li>
-			<li>menu.php</li>
-			<li>Profile.css</li>
-=======
-			<li>menu.php</li>
->>>>>>> Stashed changes
-			<li>User_Profile.php</li>
-		</p>
-	<h2><u>Angie Rybalko</u></h2>
-		<p>
-			<li>ack.php</li>
-			<li>add.php</li>
-			<li>bookInfo.js</li>
-			<li>bookInfo.php</li>
-			<li>bookSearchPage.php</li>
-			<li>bookInfo.css</li>
-			<li>booksearch.php</li>
-			<li>loginPage.php</li>
-<<<<<<< Updated upstream
-			<li>menu.css</li>
-			<li>menu.php</li>
-		</p>
-	</div>
-=======
-			<li>menu.php</li>
-		</p>
-	</div>
-	<div>
-		<p>
-			* All authors contributed to creating and populating the tables in the database used for the 
-			back end portion of the project.
-		</p>
-	</div>
-	
->>>>>>> Stashed changes
-	
-</body>
+	<body>
+		
+		<?php
+			include("menu.php")
+		?>
+		
+		<div class= box>
+			<h2><u>Lindsey Arndt</u></h2>
+			<p>
+				<li>bookInfo.php</li>
+				<li>bookSearchPage.php</li>
+				<li>bookInfo.css</li>
+				<li>booksearch.php</li>
+				<li>menu.php</li>
+				
+			</p>
+
+			<h2><u>Ahlea Wright</u></h2>
+			<p>
+				<li>ack.php</li>
+				<li>bookInfo.css</li>
+				<li>booksearch.php</li>
+				<li>loginPage.php</li>
+				<li>menu.php</li>
+			</p>
+
+			<h2><u>Karis Plath</u></h2>
+			<p>
+				<li>ack.php</li>
+				<li>bookInfo.php</li>
+				<li>booksearch.php</li>
+				<li>menu.php</li>
+				<li>User_Profile.php</li>
+			</p>
+
+			<h2><u>Angie Rybalko</u></h2>
+			<p>
+				<li>ack.php</li>
+				<li>add.php</li>
+				<li>bookInfo.js</li>
+				<li>bookInfo.php</li>
+				<li>bookSearchPage.php</li>
+				<li>bookInfo.css</li>
+				<li>booksearch.php</li>
+				<li>loginPage.php</li>
+				<li>menu.php</li>
+			</p>
+		</div>
+
+		<div>
+			<p>
+				* All authors contributed to creating and populating the tables in the database used for the 
+				back end portion of the project.
+			</p>
+		</div>
+		
+		
+	</body>
 </html>
