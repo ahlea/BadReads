8673a36d5e518ba74c93ce312ca90ff7f79990f8
Ahlea Wright <wrightahlea@gmail.com>
2022-11-30 19:07:24 -0600

    Update ack.php
    bullet list of contributions

--- ack.php
+++ ack.php
@@ -14,13 +14,46 @@
 	
 	<div class= box>
 	<h2><u>Lindsey Arndt</u></h2>
-		<p>Created the login page and is generally awesome</p>
+		<p>
+			<li>bookInfo.php</li>
+			<li>bookSearchPage.php</li>
+			<li>bookInfo.css</li>
+			<li>booksearch.php</li>
+			<li>menu.php</li>
+			
+		</p>
 	<h2><u>Ahlea Wright</u></h2>
-		<p>Created the book search page and id column to link to database. Resident redhead</p>
+		<p>
+			<li>ack.php</li>
+			<li>bookInfo.css</li>
+			<li>booksearch.php</li>
+			<li>login.css</li>
+			<li>loginPage.php</li>
+			<li>menu.php</li>
+		</p>
 	<h2><u>Karis Plath</u></h2>
-		<p>Menu bar/user profile/added books to database. Looks like Angie.</p>
+		<p>
+			<li>ack.php</li>
+			<li>bookInfo.php</li>
+			<li>booksearch.php</li>
+			<li>menu.css</li>
+			<li>menu.php</li>
+			<li>Profile.css</li>
+			<li>User_Profile.php</li>
+		</p>
 	<h2><u>Angie Rybalko</u></h2>
-		<p>Book info page. Looks like Karis.</p>
+		<p>
+			<li>ack.php</li>
+			<li>add.php</li>
+			<li>bookInfo.js</li>
+			<li>bookInfo.php</li>
+			<li>bookSearchPage.php</li>
+			<li>bookInfo.css</li>
+			<li>booksearch.php</li>
+			<li>loginPage.php</li>
+			<li>menu.css</li>
+			<li>menu.php</li>
+		</p>
 	</div>
 	
 </body>
