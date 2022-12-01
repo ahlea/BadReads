3846b636e4fa476054d7fbc69a12813699ef6f8d
karissa-plathe <karisplath@gmail.com>
2022-12-01 02:24:39 -0600

    style and spacing

--- User_Profile.php
+++ User_Profile.php
@@ -1,35 +1,33 @@
 <!DOCTYPE html>
 
-<html lang = "en">
-    <head>
-        <meta charset="UTF-8">
+<html style= "background-color: #8f8f8f; color: antiquewhite">
 
-        <meta name="author" content="Karis Plath">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Karis Plath">
+    <title> Profile </title>
+    <script src="userProfile.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="BookInfo.css">
+  </head>
 
-        <title> Profile </title>
-        <script src="userProfile.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <link rel="stylesheet" href="BookInfo.css">
-    </head>
-
-    <body>
+  <body>
         
-        <?php
-          include("menu.php")
-        ?>
+    <?php
+      include("menu.php")
+    ?>
 
-          <div class="tab">
-            <button class="tablinks" type="button" onclick="showBooks('Reading')">Reading</button>
-            <button class="tablinks" type="button" onclick="showBooks('Read')">Read</button>
-            <button class="tablinks" type="button" onclick="showBooks('Wishlist')">Wishlist</button>
-          </div>
+    <div class="tab">
+      <button class="tablinks" type="button" onclick="showBooks('Reading')">Reading</button>
+      <button class="tablinks" type="button" onclick="showBooks('Read')">Read</button>
+      <button class="tablinks" type="button" onclick="showBooks('Wishlist')">Wishlist</button>
+    </div>
           
-          <div id="Reading" class="tabcontent">
-            <div id="currentTab">
-              User Profile <br><br>
-              Use this page to view and manage the books you have added to your library.
-            <div>
-          </div>
-        </div>
-    </body>
+    <div id="Reading" class="tabcontent">
+      <div id="currentTab">
+        User Profile <br><br>
+        Use this page to view and manage the books you have added to your library.
+      <div>
+    </div>
+  </body>
 </html>
